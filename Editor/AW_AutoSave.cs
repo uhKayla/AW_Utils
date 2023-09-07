f1d0727d0a8ffd0caf2dc6e7af8e12021a726def
uhKayla <hey@kayla.lol>
2023-09-06 21:19:47 -0400

    Auto save + BlendTreeAssets

---
+++ Editor/AW_AutoSave.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using System;
+
+[InitializeOnLoad]
+public class AW_AutoSave : MonoBehaviour
+{
+    private static DateTime lastSaveTime;
+    private static float saveIntervalMinutes = 5f; // mins between saves
+
+    static AW_AutoSave()
+    {
+        EditorApplication.playModeStateChanged += SaveOnPlayModeChange;
+    }
+
+    [MenuItem("AutoSave/Save Now")]
+    private static void SaveNow()
+    {
+        SaveScene();
+    }
+
+    private static void SaveOnPlayModeChange(PlayModeStateChange state)
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+        {
+            SaveScene();
+        }
+    }
+
+    private static void SaveScene()
+    {
+        if ((DateTime.Now - lastSaveTime).TotalMinutes >= saveIntervalMinutes)
+        {
+            lastSaveTime = DateTime.Now;
+            EditorSceneManager.SaveOpenScenes();
+            Debug.Log("Auto-Saved scene at: " + DateTime.Now.ToString());
+        }
+    }
+}
